1fef36717a0f0b196a9cdf392bddd8d3c71edc97
Cynthia Martinez <cyn.mart@outlook.com>
2017-11-07 06:29:58 +0000

    updating cart contents

--- Team_Project/shoppingcart.php
+++ Team_Project/shoppingcart.php
@@ -16,7 +16,7 @@ $conn = getDatabaseConnection();
     <head>
         
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-        <link href="css/styles.css" media="all" rel="stylesheet" type="text/css"/>
+        <link href="css/style.css" media="all" rel="stylesheet" type="text/css"/>
 
         <title> Shopping Cart </title>
         
